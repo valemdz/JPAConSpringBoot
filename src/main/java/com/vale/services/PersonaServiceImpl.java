0fbd064879b85904dc0799ca2c95eacf8c2afbc7
Valeria Torres <torresvalemdz@gmail.com>
2019-12-20 12:24:32 -0300

    Crear Persona con @Persistence Context

--- src/main/java/com/vale/services/PersonaServiceImpl.java
+++ src/main/java/com/vale/services/PersonaServiceImpl.java
@@ -1,34 +1,36 @@
 package com.vale.services;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vale.entities.Persona;
-import com.vale.repositories.PersonaRepository;
 
 
 @Service
 @Transactional( readOnly = true )
 public class PersonaServiceImpl implements PersonaService {
 	
-	@Autowired
-	PersonaRepository personaRepository;
-
+	@PersistenceContext 
+	EntityManager em;
+	
 	@Override
 	public Persona getPersona(Long id) {
-		Persona persona = personaRepository.findById(id).get();
+		Persona persona = em.find( Persona.class, id );
 		return persona;
 	}
 
-	@Transactional( readOnly = false )
+	
 	@Override
+	@Transactional( readOnly = false )
 	public Persona createPersona( Persona persona ) {
-		return personaRepository.save( persona );
+		
+		em.persist(persona);
+		
+		return persona;
 	}
 
 }
